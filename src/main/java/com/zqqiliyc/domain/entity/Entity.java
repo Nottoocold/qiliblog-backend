657cf9c1f19039c83dae5c062d80bb8cca2915ee
qili <chao0099xx@gmail.com>
2025-03-23 00:28:25 +0800

    refactor(entity): 将 entity包重命名为 domain.entity

--- src/main/java/com/zqqiliyc/domain/entity/Entity.java
+++ src/main/java/com/zqqiliyc/domain/entity/Entity.java
@@ -1,4 +1,4 @@
-package com.zqqiliyc.entity;
+package com.zqqiliyc.domain.entity;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
--- src/main/java/com/zqqiliyc/domain/entity/SysUserRole.java
+++ src/main/java/com/zqqiliyc/domain/entity/SysUserRole.java
@@ -1,4 +1,4 @@
-package com.zqqiliyc.entity;
+package com.zqqiliyc.domain.entity;
 
 import lombok.Getter;
 import lombok.Setter;
