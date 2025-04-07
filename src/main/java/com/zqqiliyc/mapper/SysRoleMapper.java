afc6cbe06bfb896dbc16288163f57909b29aa84d
qili <2468746360@qq.com>
2025-04-07 22:20:28 +0800

    feat(SysRoleMapper): 添加删除功能接口
    - 在 SysRoleMapper 接口中添加了 IDeleteMapper<SysRole> 接口- 此修改增加了删除角色的功能

--- src/main/java/com/zqqiliyc/mapper/SysRoleMapper.java
+++ src/main/java/com/zqqiliyc/mapper/SysRoleMapper.java
@@ -1,5 +1,6 @@
 package com.zqqiliyc.mapper;
 
+import com.zqqiliyc.common.IDeleteMapper;
 import com.zqqiliyc.domain.entity.SysRole;
 import io.mybatis.mapper.logical.LogicalMapper;
 
@@ -7,6 +8,6 @@ import io.mybatis.mapper.logical.LogicalMapper;
  * @author qili
  * @date 2025-04-06
  */
-public interface SysRoleMapper extends LogicalMapper<SysRole, Long> {
+public interface SysRoleMapper extends LogicalMapper<SysRole, Long>, IDeleteMapper<SysRole> {
 
 }
