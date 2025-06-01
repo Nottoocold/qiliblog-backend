c6adbe8257f9ad8645a5b18f0a6274c06e37be8d
qili <chao0099xx@gmail.com>
2025-06-01 12:55:38 +0800

    feat(mapper): 新增逻辑删除SQL优化包装器

--- src/main/java/com/zqqiliyc/mapper/SysRoleMapper.java
+++ src/main/java/com/zqqiliyc/mapper/SysRoleMapper.java
@@ -1,14 +1,17 @@
 package com.zqqiliyc.mapper;
 
+import com.zqqiliyc.config.LogicalOptimizeSqlWrapper;
 import com.zqqiliyc.domain.entity.SysRole;
 import com.zqqiliyc.mapper.base.Dao;
 import com.zqqiliyc.mapper.base.IDeleteMapper;
 import io.mybatis.mapper.logical.LogicalMapper;
+import io.mybatis.provider.SqlWrapper;
 
 /**
  * @author qili
  * @date 2025-04-06
  */
+@SqlWrapper(LogicalOptimizeSqlWrapper.class)
 public interface SysRoleMapper extends LogicalMapper<SysRole, Long>, IDeleteMapper<SysRole>, Dao {
 
 }
