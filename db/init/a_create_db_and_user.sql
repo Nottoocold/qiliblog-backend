2e7692e7347ddad969a1ee56ec55aef75f0dd798
qili <chao0099xx@gmail.com>
2025-06-22 21:20:52 +0800

    refactor(db): 拆分创建数据库和表结构脚本，并移除Docker Compose相关配置

---
+++ db/init/a_create_db_and_user.sql
@@ -0,0 +1,6 @@
+DROP DATABASE IF EXISTS `qiliblog`;
+CREATE DATABASE `qiliblog` DEFAULT CHARACTER SET utf8mb4;
+
+DROP USER IF EXISTS 'qiliblog'@'%';
+CREATE USER 'qiliblog'@'%' IDENTIFIED BY 'qiliblog001';
+GRANT ALL PRIVILEGES ON `qiliblog`.* TO 'qiliblog'@'%';
