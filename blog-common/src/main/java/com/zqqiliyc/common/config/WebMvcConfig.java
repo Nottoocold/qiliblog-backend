40fde99e648a6a94031167bc3b962be0b95d8071
qili <chao0099xx@gmail.com>
2025-07-18 21:33:21 +0800

    feat(security): 实现跨域安全配置，支持动态配置允许的域名、方法和请求头

--- blog-common/src/main/java/com/zqqiliyc/common/config/WebMvcConfig.java
+++ blog-common/src/main/java/com/zqqiliyc/common/config/WebMvcConfig.java
@@ -1,14 +1,29 @@
 package com.zqqiliyc.common.config;
 
 import com.zqqiliyc.common.config.prop.SecurityProperties;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * @author qili
  * @date 2025-07-17
  */
 @Configuration
+@RequiredArgsConstructor
 @EnableConfigurationProperties(SecurityProperties.class)
-public class WebMvcConfig {
+public class WebMvcConfig implements WebMvcConfigurer {
+    private final SecurityProperties securityProperties;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns(securityProperties.getAllowedOrigins().toArray(new String[0]))
+                .allowedMethods(securityProperties.getAllowedMethods().toArray(new String[0]))
+                .allowedHeaders(securityProperties.getAllowedHeaders().toArray(new String[0]))
+                .allowCredentials(securityProperties.getAllowCredentials())
+                .maxAge(securityProperties.getMaxAge());
+    }
 }
