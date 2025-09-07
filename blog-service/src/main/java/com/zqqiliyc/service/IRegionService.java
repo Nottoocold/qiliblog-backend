2e0e6cc92da4e4a5d7fea25832367b1f40917902
qili <chao0099xx@gmail.com>
2025-09-07 14:45:32 +0800

    feat(region): 添加地区树查询功能

--- blog-service/src/main/java/com/zqqiliyc/service/IRegionService.java
+++ blog-service/src/main/java/com/zqqiliyc/service/IRegionService.java
@@ -1,5 +1,6 @@
 package com.zqqiliyc.service;
 
+import cn.hutool.core.lang.tree.Tree;
 import com.zqqiliyc.domain.entity.SysRegion;
 
 import java.util.List;
@@ -12,19 +13,30 @@ import java.util.Optional;
 public interface IRegionService {
 
     /**
+     * 查询地区树
+     *
+     * @param level 需要的深度-1只有省，2省、市，3省、市、区/县，4省、市、区/县、街道，5省、市、区/县、街道、村
+     * @return 地区树
+     */
+    List<Tree<String>> findRegionTree(int level);
+
+    /**
      * 查询所有省
+     *
      * @return 省份列表
      */
     List<SysRegion> findProvinces();
 
     /**
      * 查询所有市
+     *
      * @return 市列表
      */
     List<SysRegion> findCities();
 
     /**
      * 查询所有市
+     *
      * @param provinceCode 省编码
      * @return 市列表
      */
@@ -32,12 +44,14 @@ public interface IRegionService {
 
     /**
      * 查询所有区/县级行政级别
+     *
      * @return 区/县列表
      */
     List<SysRegion> findDistricts();
 
     /**
      * 查询所有区/县级行政级别
+     *
      * @param cityCode 市编码
      * @return 区/县列表
      */
@@ -45,12 +59,14 @@ public interface IRegionService {
 
     /**
      * 查询所有街道
+     *
      * @return 街道列表
      */
     List<SysRegion> findStreets();
 
     /**
      * 查询所有街道
+     *
      * @param districtCode 区/县编码
      * @return 街道列表
      */
@@ -58,12 +74,14 @@ public interface IRegionService {
 
     /**
      * 查询所有村
+     *
      * @return 村列表
      */
     List<SysRegion> findVillages();
 
     /**
      * 查询所有村
+     *
      * @param streetCode 街道编码
      * @return 村列表
      */
@@ -71,6 +89,7 @@ public interface IRegionService {
 
     /**
      * 根据编码查询
+     *
      * @param code 编码
      * @return 地区
      */
