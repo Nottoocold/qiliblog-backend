e76e30a71dbf6bd8a0858eef63f807bc2e259466
qili <chao0099xx@gmail.com>
2025-09-27 11:03:33 +0800

    feat(cache): 引入caffeine

--- blog-zpub/src/main/java/com/zqqiliyc/BlogApplication.java
+++ blog-zpub/src/main/java/com/zqqiliyc/BlogApplication.java
@@ -3,12 +3,20 @@ package com.zqqiliyc;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.CacheManager;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.Map;
 
 @Slf4j
 @SpringBootApplication
 public class BlogApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BlogApplication.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(BlogApplication.class, args);
+        Map<String, CacheManager> cacheManagerMap = context.getBeansOfType(CacheManager.class);
+        for (Map.Entry<String, CacheManager> entry : cacheManagerMap.entrySet()) {
+            log.info("CacheManager: {}, type: {}", entry.getKey(), entry.getValue().getClass());
+        }
     }
 }
