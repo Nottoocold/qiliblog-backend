5623a521778029e3380f700d83b0cdaa19f11b81
七里之外 <chao0099xx@gmail.com>
2025-11-23 20:05:47 +0800

    chore(tests): 清理测试初始化SQL脚本格式

--- blog-module-biz/src/test/resources/test-init.sql
+++ blog-module-biz/src/test/resources/test-init.sql
@@ -1,16 +1,10 @@
 -- 创建测试账号
-CREATE
-USER qiliblog_tester WITH LOGIN PASSWORD 'qiliblog@1';
+CREATE USER qiliblog_tester WITH LOGIN PASSWORD 'qiliblog@1';
 GRANT qiliblog_role_readwrite TO qiliblog_tester;
 
 -- 创建测试schema
 CREATE SCHEMA qiliblog_test AUTHORIZATION qiliblog_owner;
 
 -- 授权ROLE相关SCHEMA访问权限。
-GRANT
-USAGE
-ON
-SCHEMA
-qiliblog_test TO qiliblog_role_readwrite;
-GRANT USAGE ON SCHEMA
-qiliblog_test TO qiliblog_role_readonly;
+GRANT USAGE ON SCHEMA qiliblog_test TO qiliblog_role_readwrite;
+GRANT USAGE ON SCHEMA qiliblog_test TO qiliblog_role_readonly;
